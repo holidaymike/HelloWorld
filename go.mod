bb026b5869068991148dfbef5818d0ed62327660
Michael Chiang <michael_chiang@cable.comcast.com>
2020-09-16 10:29:01 -0700

    stuffs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/holidaymike/HelloWorld
+
+go 1.14
+
+require github.com/holidaymike/HelloLib v1.6.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/holidaymike/HelloLib"
+)
+
+func main() {
+	fmt.Println(HelloLib.HelloString())
+}
